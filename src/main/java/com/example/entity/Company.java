b8977d887fdf5d6d02609869e2e77cb25e51a23f
janny <13469946131@163.com>
2019-03-06 11:44:51 +0800

    添加getter和setter方法

--- src/main/java/com/example/entity/Company.java
+++ src/main/java/com/example/entity/Company.java
@@ -13,4 +13,28 @@ public class Company {
     private String stress;
 
     private Integer num;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStress() {
+        return stress;
+    }
+
+    public void setStress(String stress) {
+        this.stress = stress;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
 }
