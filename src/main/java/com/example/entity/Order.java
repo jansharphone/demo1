3a82d257c61b00b2f85473b32546bb8e5cd1b7b3
janny <13469946131@163.com>
2019-03-07 14:21:33 +0800

    Merge remote-tracking branch 'remotes/github/master' into HEAD
    # Conflicts:
    #	src/main/java/com/example/entity/Company.java

--- src/main/java/com/example/entity/Order.java
+++ src/main/java/com/example/entity/Order.java
@@ -9,4 +9,12 @@ package com.example.entity;
 public class Order {
 
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
